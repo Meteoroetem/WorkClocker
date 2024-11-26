1eb371994bf0309f61b8f4aca5dc62d6d8edc538
Rotem Kassif <rotem.kassif@gmail.com>
2024-11-27 01:38:43 +0200

    initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,31 @@
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+
+namespace WorkClocker;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : AppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+        var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+        bottomNavigation!.ItemSelected += (sender, args) =>{
+            switch(args.Item.ItemId){
+                case Resource.Id.homeButton:
+                    //TODO: Switch to home page
+                   break;
+                case Resource.Id.settingsButton:
+                    //TODO: Switch to settings page
+                    break;
+                case Resource.Id.manualAddButton:
+                   //TODO: Switch to manual add page
+                   break;
+                default:
+                    break;
+            }
+        };
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Work Clocker - document your work hours
+
+## Building the project
+Add a file named `AndroidBuild.props` to the base folder. Create a C# project with the Android Sdk local build properties such as AcceptAndroidSDKLicenses, AndroidSdkDirectory and JavaSdkDirectory (make sure it's pointing to java 17!)
