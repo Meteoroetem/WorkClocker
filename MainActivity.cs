233c23dd92a975e89c884a1064f75fe03d221279
Rotem Kassif <rotem.kassif@gmail.com>
2025-01-05 21:57:08 +0200

    Dont remember

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,33 +1,54 @@
+using Android.Net.Http;
+using Android.Views;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.BottomNavigation;
-
+using Google.Android.Material.BottomSheet;
+using Android.Util;
+using AndroidX.Window;
+using System.ComponentModel;
 namespace WorkClocker;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : AppCompatActivity
 {
+    class CustomCallback : BottomSheetBehavior.BottomSheetCallback
+    {
+        AppCompatActivity _containingActivity;
+        public CustomCallback(AppCompatActivity containingActivity){
+            _containingActivity = containingActivity;
+        }
+        public override void OnSlide(View bottomSheet, float newState){}
+        
+
+        public override void OnStateChanged(View view, int newState)
+        {
+            var button = _containingActivity.FindViewById<Button>(Resource.Id.bottomsheet_button);
+            button!.Text = newState switch{
+                BottomSheetBehavior.StateCollapsed => "State Collapsed",
+                BottomSheetBehavior.StateDragging => "State Dragging",
+                BottomSheetBehavior.StateExpanded => "State Expanded",
+                BottomSheetBehavior.StateHalfExpanded => "State Half Expanded",
+                BottomSheetBehavior.StateHidden => "State Hidden",
+                BottomSheetBehavior.StateSettling => "State Settling",
+                _ =>throw new ArgumentOutOfRangeException($"Not expected state: {newState}")
+            };
+        }
+    }
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
+
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
-        /*
-            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
-            bottomNavigation!.ItemSelected += (sender, args) =>{
-                switch(args.Item.ItemId){
-                    case Resource.Id.homeButton:
-                        //TODO: Switch to home page
-                       break;
-                    case Resource.Id.settingsButton:
-                        //TODO: Switch to settings page
-                        break;
-                    case Resource.Id.manualAddButton:
-                       //TODO: Switch to manual add page
-                       break;
-                    default:
-                        break;
-                }
-            };
-        */
+
+
+        var bottomSheet = FindViewById<FrameLayout>(Resource.Id.bottomSheet);
+        var sheetBehaviour = BottomSheetBehavior.From(bottomSheet!);
+        sheetBehaviour.Hideable = false;
+        DisplayMetrics displayMetrics = new();
+        Display!.GetMetrics(displayMetrics);
+        sheetBehaviour.PeekHeight = displayMetrics.HeightPixels * 1/4;
+        sheetBehaviour.MaxHeight = displayMetrics.HeightPixels * 2/3;
+        sheetBehaviour.AddBottomSheetCallback(new CustomCallback(this));
     }
 }
---
+++ WorkDay.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkClocker
+{
+    public class WorkDay
+    {
+        public DateOnly Date {get; init;}
+        public TimeSpan EnterTime {get; init;}
+        public TimeSpan ExitTime {get; init;}
+        public TimeSpan DayLength {
+            get => ExitTime - EnterTime;
+        }
+        public WorkDay(DateOnly date, TimeSpan enterTime, TimeSpan exitTime){
+            Date = date;
+            EnterTime = enterTime;
+            ExitTime = exitTime;
+            
+        }
+    }
+}
